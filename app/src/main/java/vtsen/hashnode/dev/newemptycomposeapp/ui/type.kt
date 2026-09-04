6b7200ebb535182c0436316b801f4d9f8caf51e7
steven750MC <Yasinmazahery@yahoo.com>
2026-09-04 15:51:04 +0330

    Create type.kt

---
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/type.kt
@@ -0,0 +1,65 @@
+package vtsen.hashnode.dev.newemptycomposeapp.ui
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import vtsen.hashnode.dev.newemptycomposeapp.ui.R
+
+
+val AppFontFamily = FontFamily(
+    Font(R.font.font, FontWeight.Normal),
+    Font(R.font.font, FontWeight.Medium),
+    Font(R.font.font, FontWeight.Bold),
+    Font(R.font.font, FontWeight.Light)
+)
+
+
+val AppTypography = Typography(
+    
+    bodyLarge = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = AppFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp
+    )
+)
